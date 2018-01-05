0c080d5af8bd1507ca8cc7000b3237e477a9f9e4
Lauri Piispanen <lauri.piispanen@reaktor.com>
2018-01-05 08:20:12 +0200

    add some explanatory comments

--- plot2.R
+++ plot2.R
@@ -5,9 +5,12 @@ data <- fread("awk 'NR==1 || /^(1|2)\\/2\\/2007;/' household_power_consumption.t
 data$Weekday <- weekdays(as.POSIXlt(data$Date, format="%d/%m/%Y"), abbreviate = TRUE)
 
 # Prepare x axis labels
-xlabels_at <- c(which(!duplicated(data$Date)), nrow(data) + 1)
+# 1. merge dates so that only the first unique date in subsequent rows is selected
+# 2. add the "next date" to label vectors to "close off" the x label
+uniq_date_logical <- !duplicated(data$Date)
+xlabels_at <- c(which(uniq_date_logical), nrow(data) + 1)
 next_day <- weekdays(as.Date(as.POSIXlt(data$Date[nrow(data)], format="%d/%m/%Y")) + 1, abbreviate = TRUE)
-xlabels <- c(data$Weekday[!duplicated(data$Date)], next_day)
+xlabels <- c(data$Weekday[uniq_date_logical], next_day)
 
 png('plot2.png')
 plot(data$Global_active_power, type = "l", 
